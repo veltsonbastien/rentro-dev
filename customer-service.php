ed4a1f47ba5aa843f0908f3eb815252b9735c168
Zonkman <Zonkman@users.noreply.github.com>
2019-01-11 13:48:45 -0800

    customer service page edit
    buttons now have text and good alignment

--- customer-service.php
+++ customer-service.php
@@ -9,23 +9,23 @@
         </div>
         <div class = "container-fluid"> 
            <h1 class = "display-1"> Customer Services </h1>
-           <h3 class = "showme-text" style = "text-align: CENTER;"> Unfortunately, sometimes things don't go as planned. We're here to help you with that. </h3>
+           <h3 class = "showme-text" style = "text-align: CENTER;"> Unfortunately, sometimes things don't go as planned, but we're here to help you with that. </h3>
            <h4 class = "CS-top-text"> How can we help? </h4> 
            <ul class = "CS-ul"> 
               <li class = "CS-li"> 
                   <i class="far fa-file"></i>
                   <h3 class = "CS-li-h3">File a Report</h3> 
-                  <p class = "CS-li-p"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa voluptate, adipisci quas pariatu </p> 
+                  <p class = "CS-li-p"> Send us a text report via Rentro.</p> 
               </li> 
               <li class = "CS-li">
                   <i class="fas fa-phone-square"></i>
                   <h3 class = "CS-li-h3">Schedule a Call</h3> 
-                  <p class = "CS-li-p"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa voluptate, adipisci quas pariatu </p>     
+                  <p class = "CS-li-p"> Find a time to contact us directly via phone. </p>     
               </li>
               <li class = "CS-li">
                   <i class="far fa-envelope"></i>
                   <h3 class = "CS-li-h3">Send an Email</h3> 
-                  <p class = "CS-li-p"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa voluptate, adipisci quas pariatu </p> 
+                  <p class = "CS-li-p"> Send us a text report via e-mail. </p> 
               </li> 
             </ul>
          </div>
