bda778d4937a7c4aa4a90308a1381b30bcdc8eb9
Zonkman <Zonkman@users.noreply.github.com>
2019-01-11 13:28:43 -0800

    buyer/seller info fixes and minor changes
    basically, i changed some text to be more concise.
    also there are links connected to the words "create an account" so that newcomers don't have to navigate the menus, even.
    finally, I thought the first time the three steps are stated were kind of redundant, so I commented them out; but if you want to you can get it back

--- buyer-information.php
+++ buyer-information.php
@@ -10,15 +10,15 @@
         <div class = "container-fluid"> 
            <h1 class = "display-1"> What you need to know as a buyer </h1>
            <img id = "SI-img" src = "images/image2.jpg"> 
-           <h4 class = "title-text"> Buying on Rentro is the best way to find great devices at low prices, and you could do it in three simple steps: 
-                                     <br> <br> Buy, Enjoy, and Return.   </h4>
+           <h4 class = "title-text"> Buying on Rentro is the best way to find great devices at low prices, and you can do it in three simple steps: 
+                                     <br> <br> <!--Buy, Enjoy, and Return.   --></h4>
            <p class = "paragraph-text"> </p> 
            <ul class = "SI-info-boxes"> 
               <li class = "SI-info-boxes-li"> 
                   <span class = "SI-info-boxes-span">
                    <i class="fas fa-shopping-cart"></i>
                    <h3 class = "SI-infoboxes-h3"> Buy </h3> 
-                   <p class = "SI-info-boxes-p"> If you haven't already, create an account. Next, browse our collection of items that have been put up for rent, and choose what it is you would like to rent out. There will be more directions and information when you are ready to purchase.  </p> 
+                   <p class = "SI-info-boxes-p"> If you haven't already, <a class = "SI-inline-link" href = "create-account.php">create an account.</a> Next, browse our collection of items that have been put up for rent, and choose what it is you would like to rent out. There will be more directions and information when you are ready to purchase.  </p> 
                   </span>
               </li>
               <li class = "SI-info-boxes-li"> 
